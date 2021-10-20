743c883a92d86c5f6827e0f960c96d8badbbd4f5
Sagie Gur-Ari <sagiegurari@gmail.com>
2021-10-20 17:32:10 +0000

    docs and tests

--- test/spec/integration-spec.js
+++ test/spec/integration-spec.js
@@ -6,7 +6,7 @@ const integrationHelper = require('../helpers/integration-helper');
 const utils = require('../helpers/utils');
 
 integrationHelper(function (oracledb, connAttrs, initDB, end) {
-    if (oracledb && connAttrs && initDB && end && false) {
+    if (oracledb && connAttrs && initDB && end) {
         describe('Integration Tests', function () {
             const self = this;
 
