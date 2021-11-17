e6341ab83f99907893b3d61b0f21fbaae4806448
Sagie Gur-Ari <sagiegurari@gmail.com>
2021-11-17 16:51:16 +0000

    Additional stability test #34

--- test/spec/stability-spec.js
+++ test/spec/stability-spec.js
@@ -69,6 +69,76 @@ if (process.env.TEST_STABILITY) {
                         });
                     });
                 });
+
+                it('update - LOB data', function (done) {
+                    const table = 'TEST_ORA_STB2';
+
+                    let longClobText = 'this is a really long line of test data\n';
+                    const buffer = [];
+                    for (let index = 0; index < 1000; index++) {
+                        buffer.push(longClobText);
+                    }
+                    longClobText = buffer.join('');
+
+                    initDB(table, [], function (pool) {
+                        pool.getConnection(function (err, connection) {
+                            assert.isNull(err);
+
+                            connection.insert(`INSERT INTO ${table} (COL1, COL2, LOB1, LOB2) values (:value1, :value2, EMPTY_CLOB(), EMPTY_BLOB())`, {
+                                value1: 'test',
+                                value2: 123,
+                                clob1: longClobText,
+                                blob2: utils.createBuffer('blob text here')
+                            }, {
+                                autoCommit: true,
+                                lobMetaInfo: {
+                                    LOB1: 'clob1',
+                                    LOB2: 'blob2'
+                                }
+                            }, function (error, results) {
+                                assert.isNull(error);
+                                assert.equal(1, results.rowsAffected);
+
+                                connection.release(() => {
+                                    pool.run(function (runConnection, cb) {
+                                        runConnection.update(`UPDATE ${table} SET LOB1 = EMPTY_CLOB() WHERE COL1 = :value1`, {
+                                            newclob1: 'NEW CLOB TEXT VALUE',
+                                            value1: 'test',
+                                        }, {
+                                            autoCommit: true,
+                                            lobMetaInfo: {
+                                                LOB1: 'newclob1'
+                                            }
+                                        }, cb);
+                                    }, function onUpdate(updateError, updateResults) {
+                                        assert.isNull(updateError);
+                                        assert.isDefined(updateResults);
+
+                                        pool.run(function (runConnection, cb) {
+                                            runConnection.query(`SELECT * FROM ${table}`, [], {
+                                                resultSet: false
+                                            }, cb);
+                                        }, function (queryError, jsRows) {
+                                            assert.isNull(queryError);
+                                            assert.deepEqual([
+                                                {
+                                                    COL1: 'test',
+                                                    COL2: 123,
+                                                    COL3: null,
+                                                    COL4: null,
+                                                    LOB1: 'NEW CLOB TEXT VALUE',
+                                                    LOB2: utils.createBuffer('blob text here')
+                                                }
+                                            ], jsRows);
+
+                                            done();
+                                        });
+                                    });
+                                });
+                            });
+                        });
+                    });
+                });
             });
         }
     });
